e0e50f9175b2457773a87dd2f557a848053f1749
MMayilyan <80274024+MMayilyan@users.noreply.github.com>
2025-04-28 17:27:55 +0400

    Added database homework scripts

---
+++ database.sql
@@ -0,0 +1,22 @@
+create Schema Homework;
+
+create table company (
+	CompanyName varchar(255),
+    Industry varchar(255),
+    Website varchar(255),
+    Adress text,
+    ContactEmail varchar(255),
+    PhoneNumber varchar(50)
+);
+
+insert into company (CompanyName, Industry, Website, Adress, ContactEmail, PhoneNumber) values 
+	('TechWave Solutions', 'Technology', 'www.techwave.com', '123 Silicon Valley Blvd San Jose CA 95129', 'contact@techwave.com', '+1 (408) 555-1212'),
+	('GreenLeaf Organics', 'Agriculture', 'www.greenleaforganics.co', '786 Greenway St Austin TX 78701', 'info@greenleaforganics.co', '+1 (512) 555-3456'),
+	('SkyHigh Aerospace', 'Aerospace', 'www.skyhighaero.com', '980 Flight Dr Seattle WA 98109', 'support@skyhighaero.com', '+1 (206) 555-9876'),
+	('Global Financial Group', 'Finance', 'www.globalfinancial.com', '455 Wall St New York NY 10005', 'nquiry@globalfinancial.com', '+1 (212) 555-2233'),
+	('BrightPath Education', 'Education', 'www.brightpathedu.org', '67 Scholar Ave Boston MA 02118', 'admissions@brightpathedu.org', '+1 (617) 555-8901');
+
+select * from company;
+
+set SQL_SAFE_UPDATES = 0;
+delete from company where CompanyName = 'GreenLeaf Organics';
